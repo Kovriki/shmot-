3b55373634234448049a104e3961eeeadf3962f1
Kovriki <polinamazitova@gmail.com>
2021-10-23 15:51:49 +0300

    у

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_23_114018) do
+ActiveRecord::Schema.define(version: 2021_10_23_120518) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 2021_10_23_114018) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
 end
