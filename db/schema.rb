1e9a9c740e4c940118bb0f042631a5714e5f44bf
Kovriki <polinamazitova@gmail.com>
2021-10-23 13:18:15 +0300

    main design + images

--- db/schema.rb
+++ db/schema.rb
@@ -33,5 +33,6 @@ ActiveRecord::Schema.define(version: 2021_10_23_014624) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
+  
 
 end
